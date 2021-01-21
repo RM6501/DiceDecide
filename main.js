f30cce139ba2f3fe3f77abea435d5cfbba8b7aba
Rohan Makwana <67287822+RM6501@users.noreply.github.com>
2021-01-21 17:54:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var randomNumber1=Math.ceil(Math.random()*6);
+console.log(randomNumber1);
+
+var randomDice1=document.querySelector(".img1").getAttribute("src");
+console.log(randomDice1);
+var changeImg1="images/dice"+randomNumber1+".png";
+var image=document.querySelector(".img1").setAttribute("src",changeImg1);
+
+var randomNumber2=Math.ceil(Math.random()*6);
+console.log(randomNumber2);
+
+var randomDice2=document.querySelector(".img2").getAttribute("src");
+console.log(randomDice2);
+var changeImg2="images/dice"+randomNumber2+".png";
+var image=document.querySelector(".img2").setAttribute("src",changeImg2);
+
+
+
+   if(randomNumber1 > randomNumber2){
+     document.querySelector("h1").textContent="🎊Player1 Wins🎉";
+   }
+   else if (randomNumber1 == randomNumber2) {
+     document.querySelector("h1").textContent="🎊Player2 Wins🎉";
+   }
+   else {
+      document.querySelector("h1").textContent="DRAW!!😂";
+   }
